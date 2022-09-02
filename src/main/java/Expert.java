1a69076a7aadaee23c2216c66d260a350c2cb0e1
hunjoonrhee <hunjoonrhee90@gmail.com>
2022-09-02 09:34:04 +0200

    Modified reverse method

--- src/main/java/Expert.java
+++ src/main/java/Expert.java
@@ -1,6 +1,20 @@
 public class Expert {
 
     public static String reverse(String string){
-        return string;
+        String result = "";
+        if(string == null){
+            result = "Die Eingabe darf nicht null sein!";
+        }else{
+            char[] c = string.toCharArray();
+            if(c.length == 1){
+                result = string;
+            }else{
+                for(int i = c.length-1; i>=0 ; i--){
+                    result += c[i];
+                }
+            }
+        }
+
+        return result;
     }
 }
